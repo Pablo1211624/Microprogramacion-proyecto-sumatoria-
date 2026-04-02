798d91def859cf76fd3a85ca045bec587023ac17
Pablo1211624 <pjgonzalezp@correo.url.edu.gt>
2026-04-02 16:24:39 -0600

    version final

--- Proyecto.asm
+++ Proyecto.asm
@@ -1,194 +1,195 @@
 .model small
+.386
 .data
     A DB 1
-    I DB 1
-    B DB ?
-    SIMBOLO DB ?
+    I DB 0
+    I_VALOR DB 0
+    B DB 0
+    N DB 0
+    SIMBOLO DB '+'
     TOTAL DW 0
-    AXVAL DW 0
-    ERRORESTRUCURA DB 'Error en la estructura de los paréntesis$'
-.stack 
+
+    MSGMENU DB 13,10,'1.Ingresar funcion',13,10
+            DB '2.Ingresar n',13,10
+            DB '3.Ingresar i',13,10
+            DB '4.Valuar',13,10
+            DB '5.Salir',13,10,'$'
+
+    MSGFUNC  DB 13,10,'Ingrese funcion: $'
+    MSGN     DB 13,10,'Ingrese N: $'
+    MSGI     DB 13,10,'Ingrese I inicial: $'
+    SALTO    DB 13,10,'$'
+
+.stack 100h
 .code
 
 programa:
-;Iniciar el programa
-    MOV AX, @data ; Cargar la dirección del segmento de datos en AX
-    MOV DS, AX ; Establecer DS para acceder a las variables
+    MOV AX,@data
+    MOV DS,AX
 
-    MOV A, 1 ; Inicializar A con un valor predeterminado
+MENU:
+    MOV DX,OFFSET MSGMENU
+    MOV AH,9
+    INT 21H
+    CALL LEER
+    CMP AL,'1'
+    JE FUNCION
+    CMP AL,'2'
+    JE LEERN
+    CMP AL,'3'
+    JE LEERI
+    CMP AL,'4'
+    JE SUMATORIA
+    CMP AL,'5'
+    JE FIN
+    JMP MENU
 
-; Iniciar contador de A
-    MOV CL, 3
-; Leer el valor de A
-LeerA:
+LEER:
+    MOV AH,08H
+    INT 21H
+    CMP AL,13
+    JE LEER
+    MOV DL,AL
+    MOV AH,2
+    INT 21H
+    RET
 
-    XOR AX, AX ; Limpiar el registro AX
-    MOV AH, 1H ; Función para leer un carácter
-    INT 21H ; Interrupción para leer el carácter
+FUNCION:
+    MOV DX,OFFSET MSGFUNC
+    MOV AH,9
+    INT 21H
+    CALL LEER
+    CMP AL,'('
+    JE ES_SIN_A
+    SUB AL,'0'
+    MOV A,AL
+    CALL LEER ; Leer '('
+ES_SIN_A:
+    CALL LEER ; Leer 'i'
+    CALL LEER ; Leer el operador (+, -, *, /, M)
+    MOV SIMBOLO,AL
+    
+    CMP AL,'M'
+    JNE SIGUE_B
+    CALL LEER ; Leer 'O'
+    CALL LEER ; Leer 'D'
 
-    CMP AL, '9' ; Comparar con '9'
-    JG  ErrorEstructura ; Si es mayor que '9', tirar excecpcion y reiniciar el programa
-    CMP AL, '0' ; Comparar con '0'
-    JL LeerParentesis  
-    ;Restar el valor ASCII de '0' para obtener el valor numérico
-    SUB AL, '0'
-    ;Comparar contador para ver si hay que multiplicar
-    CMP CL, 2
-    JE Multiplicar10
-    CMP CL, 1
-    JE Multiplicar10 
-    ;Ubicar el valor de A
-    MOV A, AL
-    LOOP LeerA ; Se puede leer a lo mucho 3 digitos para A
-    JMP LeerParentesis
+SIGUE_B:
+    CALL LEER ; Leer número B
+    SUB AL,'0'
+    MOV B,AL
+    CALL LEER ; Leer ')'
+    JMP MENU
 
-; Toma por hecho que el registro AL tiene el valor
-Multiplicar10:
-    MOV BL, 10
-    MOV DL, AL 
-    MOV AL, A
+LEERN:
+    MOV DX,OFFSET MSGN
+    MOV AH,9
+    INT 21H
+    CALL LEER
+    SUB AL,'0'
+    MOV N,AL
+    JMP MENU
+
+LEERI:
+    MOV DX,OFFSET MSGI
+    MOV AH,9
+    INT 21H
+    CALL LEER
+    SUB AL,'0'
+    MOV I_VALOR,AL
+    JMP MENU
+
+SUMATORIA:
+    MOV AL, I_VALOR
+    MOV I, AL
+    MOV TOTAL, 0
+
+CICLO:
+    XOR AX, AX
+    MOV AL, I
+    MOV BL, B
+
+    ; --- LÓGICA DE OPERACIONES ---
+    CMP SIMBOLO, '+'
+    JE HACE_SUMA
+    CMP SIMBOLO, '-'
+    JE HACE_RESTA
+    CMP SIMBOLO, '*'
+    JE HACE_MULT
+    CMP SIMBOLO, '/'
+    JE HACE_DIVI
+    CMP SIMBOLO, 'M'
+    JE HACE_MOD
+    JMP CONTINUA
+
+HACE_SUMA:
+    ADD AL, BL
+    JMP CONTINUA
+HACE_RESTA:
+    SUB AL, BL
+    JMP CONTINUA
+HACE_MULT:
     MUL BL
-    XOR A, A
-    MOV A, AL
-    ADD A, DL
-    CMP A, 255 ; Verificar si el resultado excede el rango de un byte
-    JG ErrorEstructura ; Si es mayor que 255, tirar excepción y reiniciar el programa
-    LOOP LeerA
-; Leer parentesis
-LeerParentesis:
-    XOR AX, AX ; Limpiar el registro AX
-    MOV AH, 1H  
+    JMP CONTINUA
+HACE_DIVI:
+    XOR AH, AH
+    DIV BL
+    JMP CONTINUA
+HACE_MOD:
+    XOR AH, AH
+    DIV BL
+    MOV AL, AH ; El residuo queda en AH, lo pasamos a AL para operar
+
+CONTINUA:
+    ; Aplicar el multiplicador A que está afuera del paréntesis
+    XOR AH, AH
+    MOV BL, A
+    MUL BL
+    
+    MOV BX, AX
+    ADD TOTAL, BX
+    
+    MOV AX, BX
+    CALL IMPRIMIR
+
+    MOV AL, I
+    CMP AL, N
+    JAE RESULTADO_FINAL
+
+    MOV DL, '+'
+    MOV AH, 2
     INT 21H
 
-    CMP AL, '(' ; Comparar con '('
-    JE LeerSimbolo
-    CMP AL, ')' ; Comparar con ')'
-    JE SUMATORIAJMP
-    JMP ErrorEstructura
+    INC I
+    JMP CICLO
 
-;Lee el simbolo de la operacion    
-LeerSimbolo:
-    XOR AX, AX ; Limpiar el registro AX
-    MOV AH, 1H
+RESULTADO_FINAL:
+    MOV DX, OFFSET SALTO
+    MOV AH, 9
     INT 21H
+    MOV AX, TOTAL
+    CALL IMPRIMIR
+    JMP MENU
 
-    CMP AL, '+' ; Comparar con '+'
-    JE SUMAJMP
-    CMP AL, '-' ; Comparar con '-'
-    JE RESTAJMP
-    CMP AL, '*' ; Comparar con '*'
-    JE MULTIPLICACIONJMP
-    CMP AL, '/' ; Comparar con '/'
-    JE DIVISIONJMP
-    CMP AL, 'm' ; Comparar con 'm'
-    JE MODJMP
-    JMP ErrorEstructura
-
-SUMAJMP:
-    MOV SIMBOLO, AL ; Guardar el símbolo de la operación
-    JMP LEERB
-RESTAJMP:
-    MOV SIMBOLO, AL ; Guardar el símbolo de la operación
-    JMP LEERB
-MULTIPLICACIONJMP:
-    MOV SIMBOLO, AL ; Guardar el símbolo de la operación
-    JMP LEERB
-DIVISIONJMP:
-    MOV SIMBOLO, AL ; Guardar el símbolo de la operación
-    JMP LEERB
-; MODJMP:
-;     MOV SIMBOLO, AL ; Guardar el símbolo de la operación
-
-;Lee el numero despues de simbolo
-LEERB:
-    XOR AX, AX ; Limpiar el registro AX
-    MOV AH, 1H
+IMPRIMIR:
+    MOV BX, 10
+    MOV CX, 0
+DIVI_IMP:
+    XOR DX, DX
+    DIV BX
+    PUSH DX
+    INC CX
+    OR AX, AX
+    JNZ DIVI_IMP
+MUESTRA_IMP:
+    POP DX
+    ADD DL, '0'
+    MOV AH, 2
     INT 21H
+    LOOP MUESTRA_IMP
+    RET
 
-    CMP AL, '9' ; Comparar con '9'
-    JG  ErrorEstructura ; Si es mayor que '9', volver al inicio del programa
-    CMP AL, '0' ; Comparar con '0'
-    JL ErrorEstructura
-    MOV B, AL
-    MOV CL, A
-    JMP LeerParentesis
-
-  
-ErrorEstructura:
-    MOV DX, OFFSET ERRORESTRUCURA
-    MOV AH, 9H ; Función para mostrar un mensaje
-    INT 21H ; Interrupción para mostrar el mensaje
-    JMP programa ; Volver al inicio del programa
-
-; Compara el simbolo para realizar la operación correspondiente
-SUMATORIAJMP:
-;Servira para imprimir 
-MOV BX, 10
-MOV CX, 0
-
-;Revisar simbolos
-CMP SIMBOLO, '+' ; Comparar con '+'
-JE SUMA
-CMP SIMBOLO, '-' ; Comparar con '-'
-JE RESTA
-CMP SIMBOLO, '*' ; Comparar con '*'
-JE MULTIPLICACION
-CMP SIMBOLO, '/' ; Comparar con '/'
-JE DIVISION
-JMP ErrorEstructura
-
-
-SUMA: 
-ADD I, B
-; Hay que imprimimr el resultado de la suma que esta en AX porque podria ser de dos bytes
-JMP PRINT
-INC I 
-LOOP SUMA
-JMP FIN
-
-RESTA:
-SUB I, B
-; Hay que imprimimr el resultado de la resta que esta en AL
-INC I
-LOOP RESTA
-JMP FIN
-
-MULTIPLICACION:
-MOV AL, I
-MUL B
-; Hay que imprimimr el resultado de la multiplicacion que esta en AX porque podria ser de dos bytes
-JMP CONVERTMUL
-LOOPMULTIPLICACION:
-MOV CL, A 
-DEC A
-INC I
-LOOP MULTIPLICACION
-JMP FIN
-
-DIVISION:
-MOV AL, I
-DIV B
-MOV I, AL
-
-CONVERTMUL:
-XOR DX, DX
-DIV BX
-PUSH DX
-INC CX
-CMP AX, 0
-JNE CONVERTMUL
-
-PRINT:  
-POP DX
-ADD DL, '0'
-MOV AH, 2H
-INT 21H
-LOOP PRINT 
-CMP SIMBOLO, '*' ; Comparar con '*'
-JE LOOPMULTIPLICACION
-CMP SIMBOLO, '/' ; Comparar con '/'
-JE DIVISION
-
-
-
+FIN:
+    MOV AH, 4CH
+    INT 21H
 END programa
